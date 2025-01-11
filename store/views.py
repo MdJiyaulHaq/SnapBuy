1abd096db4e3af276b59620e2b42ac0fd6223365
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-01-11 16:01:11 +0545

    Add product detail view and update product list view to use DRF response

--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view()
 def product_list(request):
-    return HttpResponse("Hello, World!")
+    return Response("Hello, World!")
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
