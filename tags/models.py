38cf2c4e7057f1574a109864fdfde37dc289bd0c
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-01-16 20:21:47 +0545

    Refactor comments in Tag and TaggedItem models for clarity and consistency

--- tags/models.py
+++ tags/models.py
@@ -5,17 +5,21 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Define a Tag model to store tags
 class Tag(models.Model):
-    # Label of the tag with a maximum length of 255 characters
+    # Label of the tag
     label = models.CharField(max_length=255)
 
 
 # Define a TaggedItem model to associate tags with any model instance
 class TaggedItem(models.Model):
-    # ForeignKey to the Tag model, ensuring that if a tag is deleted, the related TaggedItem is also deleted
+    # what tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # ForeignKey to the ContentType model, which allows linking to any model
+    # If a tag is deleted, it should be removed from all the associated objects.
+
+    # Type (product, video, article, etc)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    # Field to store the primary key of the related model instance
+
+    # Object_ID to identify any objects or any records in any tables
     object_id = models.PositiveIntegerField()
+
     # GenericForeignKey to create a generic relation to any model instance
     content_object = GenericForeignKey()
