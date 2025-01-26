2443cea59736a653fbe4866f182fd4667ad57742
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-01-26 16:43:33 +0545

    Alter Cart model to use UUID as primary key and update migration for the change

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from autoslug import AutoSlugField
 from django.core.validators import *
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -99,6 +100,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
