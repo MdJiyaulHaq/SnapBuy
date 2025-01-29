14b8686f056cdcbb15ee4b3cb69f6308f01983ed
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-01-29 11:14:32 +0545

    Add Djoser and JWT authentication support for user management

--- storefront/urls.py
+++ storefront/urls.py
@@ -25,6 +25,8 @@ admin.site.index_title = "Admin Portal"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
     path("__debug__/", include(debug_toolbar.urls)),
     path("store/", include("store.urls")),
 ]
