efdae33d255389df5d574396d781662908f9431c
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-01-29 20:49:52 +0545

    Add authentication requirement to CustomerViewSet

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, action
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -358,6 +359,7 @@ class CustomerViewSet(
 ):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
     @action(detail=False, methods=["GET", "PUT", "PATCH"])
     def me(self, request):
@@ -375,4 +377,3 @@ class CustomerViewSet(
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data)
-
