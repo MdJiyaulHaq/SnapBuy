d9dfa8e8bde5fe7a04b55f8eb775053acf5ce195
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-02-06 12:10:08 +0545

    Custom Inline without dependency to other apps

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.contenttypes.admin import GenericTabularInline
+
+from store.models import Product
 from .models import User
+from store.admin import ProductAdmin
+from tags.models import TaggedItem
+
+
+class TagInline(GenericTabularInline):
+    model = TaggedItem
+    autocomplete_fields = ["tag"]
+    extra = 1
+
+
+class CustomProductAdmin(ProductAdmin):
+    inlines = [TagInline]
+
+
+admin.site.unregister(Product)
+admin.site.register(Product, CustomProductAdmin)
 
 
 # Register your models here.
