f9b4cb510f139597c0e5093707bb0971a08b8f30
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-02-12 15:22:05 +0545

    Refactor say_hello function to class-based view SayHelloView with caching

--- playground/views.py
+++ playground/views.py
@@ -7,14 +7,24 @@ from .tasks import notify_customers
 import requests
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
 
 
-# Create your views here.
-@cache_page(5 * 60)  # better way for caching
-def say_hello(request):
-    response = requests.get("https://httpbin.org/delay/3")
-    data = response.json()
-    return render(request, "hello.html", {"name": data})
+# If we have class based views
+class SayHelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get("https://httpbin.org/delay/3")
+        data = response.json()
+        return render(request, "hello.html", {"name": data})
+
+
+# @cache_page(5 * 60)  # better way for caching
+# def say_hello(request):
+#     response = requests.get("https://httpbin.org/delay/3")
+#     data = response.json()
+#     return render(request, "hello.html", {"name": data})
 
 
 # def say_hello(request):
