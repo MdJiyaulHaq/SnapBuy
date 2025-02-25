46c4298274fa995b8583fadddaf795ac6515e151
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-02-25 11:42:43 +0545

    Refactor URL configuration to conditionally include debug_toolbar and silk middleware in development mode

--- storefront/urls.py
+++ storefront/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
 
 admin.site.site_header = "Storefront Admin"
 admin.site.site_title = "Admin"
@@ -31,9 +30,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
-    path("__debug__/", include(debug_toolbar.urls)),
     path("store/", include("store.urls")),
 ]
+
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("silk/", include("silk.urls", namespace="silk")),
+    ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
