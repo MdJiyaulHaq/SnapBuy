1052adfe2efac3f68dd5d81912998fc4715de77d
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2025-04-09 13:42:41 +0545

    feat: add throttling configuration for product viewset

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -138,14 +138,16 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-        'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+        "rest_framework.throttling.ScopedRateThrottle",
     ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day",
+        "products": "10/minute",
+    },
 }
 
 DJOSER = {
