d415a3f59cf8e158a85a441f11e79701c1333599
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2026-01-08 23:54:56 +0545

    streamline project structure

--- storefront/celery.py
+++ storefront/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storefront.settings")
 celeryApp = Celery("storefront")
 celeryApp.config_from_object("django.conf:settings", namespace="CELERY")
 celeryApp.autodiscover_tasks()
