ed52934dded6a9ac834d0562b3787af1153101df
MdJiyaulHaq <mdjiyaulhaq78692@gmail.com>
2026-01-08 23:41:18 +0545

    django unfold admin customization

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from django.utils.html import format_html
@@ -26,7 +27,7 @@ admin.site.register(Product, CustomProductAdmin)
 
 # Register your models here.
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
     add_fieldsets = (
         (
             None,
